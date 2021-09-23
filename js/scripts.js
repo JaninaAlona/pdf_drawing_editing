091f49852290e9cfd0a938d8bbe44c4c7f741d2e
JaninaAlona <jelona@posteo.net>
2021-09-23 03:49:48 +0200

    zooming, next previous page, scrolling by page added

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,4 @@
-var myState = {
+let myState = {
     pdf: null,
     currentPage: 1,
     zoom: 1.5
@@ -8,41 +8,95 @@ var myState = {
 function render() {
     // The document is loaded here...
     myState.pdf.getPage(myState.currentPage).then(function(page) {
-        console.log('Page loaded');
 
-        var scale = myState.zoom;
-            var viewport = page.getViewport({
+        let scale = myState.zoom;
+        let viewport = page.getViewport({
             scale: scale
         });
 
-        var canvas = document.getElementById('pdf_renderer');
-        var context = canvas.getContext('2d');
+        const canvas = document.getElementById('pdf_renderer');
+        const context = canvas.getContext('2d');
         canvas.height = viewport.height;
         canvas.width = viewport.width;
 
         // Render PDF page into canvas context
-        var renderContext = {
+        let renderContext = {
             canvasContext: context,
             viewport: viewport
         };
         page.render(renderContext);
+        document.getElementById("canvas_container").style.visibility = "visible";
     });
+}
+
+
+function goPrevPage() {
+    if(myState.pdf == null || myState.currentPage == 1) {
+        return;
+    }    
+    myState.currentPage -= 1;
+    document.getElementById("current_page").value = myState.currentPage;
+    render();
+}
+
+function goNextPage() {
+    if(myState.pdf == null || myState.currentPage > myState.pdf._pdfInfo.numPages) {
+        return; 
+    }    
+    myState.currentPage += 1;
+    document.getElementById("current_page").value = myState.currentPage;
+    render();
+}
+
+function scrollPage(event) {
+    event.preventDefault;
+
+    //scrolling down
+    if (event.deltaY > 0) {
+        goNextPage();
+    
+    //scrolling up
+    } else if (event.deltaY < 0) {
+        goPrevPage();
+    }
 }
 
 
 
 document.getElementById('inputfile').onchange = function(event) {
-  var file = event.target.files[0];
-  var fileReader = new FileReader();
-  fileReader.onload = function() {
-    var typedarray = new Uint8Array(this.result);
-    console.log(typedarray);
-    const loadingTask = pdfjsLib.getDocument(typedarray);
-    loadingTask.promise.then(pdf => {
-        myState.pdf = pdf;
-        render();
-    });
+    const file = event.target.files[0];
+    const fileReader = new FileReader();
+    fileReader.onload = function() {
+        const typedarray = new Uint8Array(this.result);
+        const loadingTask = pdfjsLib.getDocument(typedarray);
+        loadingTask.promise.then(pdf => {
+            myState.pdf = pdf;
+            myState.currentPage = 1;
+            document.getElementById("current_page").value = 1;
+            render();
+        });
 
-  }
-  fileReader.readAsArrayBuffer(file);
+    }
+    fileReader.readAsArrayBuffer(file);
 }
+
+document.getElementById('zoom_in').addEventListener('click', (e) => {
+    if(myState.pdf == null || myState.zoom > 4.5) {
+        return;
+    }
+    myState.zoom += 0.2;
+    render();
+});
+
+document.getElementById('zoom_out').addEventListener('click', (e) => {
+    if(myState.pdf == null || myState.zoom < 0.5) {
+        return;
+    }
+    myState.zoom -= 0.2;
+    render();
+});
+
+document.getElementById('go_previous').addEventListener('click', goPrevPage);
+document.getElementById('go_next').addEventListener('click', goNextPage);
+
+document.getElementById('pdf_renderer').addEventListener('wheel', scrollPage);
