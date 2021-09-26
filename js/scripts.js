ef3d135ef03ea77e9fb69d0886b4cba5b896c5e4
JaninaAlona <jelona@posteo.net>
2021-09-26 14:21:54 +0200

    page settings dialog pop up added

--- js/scripts.js
+++ js/scripts.js
@@ -1,229 +1,253 @@
-let myState = {
-    pdf: null,
-    currentPage: 1,
-    zoom: 1.0
-}
+//import { PDFDocument } from 'pdf-lib'
 
-let renderInProgress = false;
+$(document).ready(function() {
+    let myState = {
+        pdf: null,
+        currentPage: 1,
+        zoom: 1.0
+    }
+
+    let renderInProgress = false;
+    const docDefinition = {};
 
 
-function render() {
+    function render() {
 
-    // The document is loaded here...
-    myState.pdf.getPage(myState.currentPage).then(function(page) {
+        // The document is loaded here...
+        myState.pdf.getPage(myState.currentPage).then(function(page) {
 
-        let scale = myState.zoom;
-        let viewport = page.getViewport({
-            scale: scale
+            let scale = myState.zoom;
+            let viewport = page.getViewport({
+                scale: scale
+            });
+
+            const canvas = document.getElementById('pdf_renderer');
+            const context = canvas.getContext('2d');
+            canvas.height = viewport.height;
+            canvas.width = viewport.width;
+
+            // Render PDF page into canvas context
+            let renderContext = {
+                canvasContext: context,
+                viewport: viewport
+            };
+
+            if (!renderInProgress) {
+                doRender(renderContext, page);
+            }
         });
+    }
 
-        const canvas = document.getElementById('pdf_renderer');
-        const context = canvas.getContext('2d');
-        canvas.height = viewport.height;
-        canvas.width = viewport.width;
 
-        // Render PDF page into canvas context
-        let renderContext = {
-            canvasContext: context,
-            viewport: viewport
-        };
+    function doRender(renderContext, page) {
+        renderInProgress = true;
+        let renderOp = page.render(renderContext);
+        renderOp.promise.then(function() {
+            document.getElementById("canvas_container").style.visibility = "visible";
+        }).finally(function() {
+            renderInProgress = false;
+        })
+    }
 
-        if (!renderInProgress) {
-            doRender(renderContext, page);
+
+    function goPrevPage() {
+        if (myState.pdf == null || myState.currentPage == 1) {
+            return;
         }
+        myState.currentPage -= 1;
+        document.getElementById("current_page").value = myState.currentPage;
+        render();
+    }
+
+    function goNextPage() {
+        if (myState.pdf == null || myState.currentPage > myState.pdf._pdfInfo.numPages) {
+            return;
+        }
+        myState.currentPage += 1;
+        document.getElementById("current_page").value = myState.currentPage;
+        render();
+    }
+
+    function scrollPage(event) {
+        event.preventDefault;
+
+        //scrolling down
+        if (event.deltaY > 0) {
+            goNextPage();
+
+            //scrolling up
+        } else if (event.deltaY < 0) {
+            goPrevPage();
+        }
+    }
+
+    function enterPageNum(event) {
+        event.preventDefault;
+
+        if (myState.pdf == null) {
+            return;
+        }
+
+        if (event.key == 'Enter') {
+            const desiredPage = document.getElementById('current_page').valueAsNumber;
+
+            if (desiredPage >= 1 && desiredPage <= myState.pdf._pdfInfo.numPages) {
+                myState.currentPage = desiredPage;
+                document.getElementById("current_page").value = desiredPage;
+                render();
+            }
+        }
+    }
+
+    function enterZoomFactor(event) {
+        event.preventDefault;
+
+        if (myState.pdf == null) {
+            return;
+        }
+
+        if (event.key == 'Enter') {
+            const desiredZoom = document.getElementById('zoom_factor').value;
+
+            let zoomVal = 0;
+            if (desiredZoom.charAt(desiredZoom.length - 1) == '%') {
+                zoomVal = parseInt(desiredZoom.substring(0, desiredZoom.length - 1));
+            } else {
+                zoomVal = parseInt(desiredZoom);
+            }
+
+            if (zoomVal >= 40 && zoomVal <= 400) {
+
+                myState.zoom = toFactor(zoomVal);
+                document.getElementById("zoom_factor").value = zoomVal + "%";
+
+                render();
+            }
+        }
+    }
+
+
+    function toPercent(factor) {
+        let strFloat = factor.toString();
+        let strFTimes100 = "";
+        let digits = strFloat.split('.');
+        if (strFloat.length == 3) {
+            if (digits[0] == '0') {
+                strFTimes100 = digits[1] + '0';
+            } else {
+                strFTimes100 = digits[0] + digits[1] + '0';
+            }
+        } else if (strFloat.length == 4) {
+            if (digits[0] == '0') {
+                strFTimes100 = digits[1] + digits[2];
+            } else {
+                strFTimes100 = digits[0] + digits[1] + digits[2];
+            }
+        } else {
+            strFTimes100 = strFloat + "00";
+        }
+        let intTimes100 = parseInt(strFTimes100);
+        return intTimes100;
+    }
+
+    function toFactor(percentage) {
+        let times10 = percentage * 100;
+        let strDiv100 = (times10 / 100).toString();
+        let strToDecimal = '';
+        if (strDiv100.length == 2) {
+            strToDecimal = "0." + strDiv100.substring(0, 1) + strDiv100.substring(1, 2);
+        } else {
+            strToDecimal = strDiv100.substring(0, 1) + '.' + strDiv100.substring(1, 2) + strDiv100.substring(2, 3);
+        }
+
+        let div100Float = parseFloat(strToDecimal);
+        return div100Float;
+    }
+
+
+    document.getElementById('inputfile').onchange = function(event) {
+        const file = event.target.files[0];
+        const fileReader = new FileReader();
+        fileReader.onload = function() {
+            const typedarray = new Uint8Array(this.result);
+            //pdfjsLib.GlobalWorkerOptions.workerSrc = '//mozilla.github.io/pdf.js/build/pdf.worker.js';
+            const loadingTask = pdfjsLib.getDocument(typedarray);
+            loadingTask.promise.then(pdf => {
+                myState.pdf = pdf;
+                myState.currentPage = 1;
+                document.getElementById("current_page").value = 1;
+                render();
+            });
+
+        }
+        fileReader.readAsArrayBuffer(file);
+    }
+
+    document.getElementById('zoom_in').addEventListener('click', (e) => {
+        if (myState.pdf == null || myState.zoom >= 4.0) {
+            return;
+        }
+
+        if (myState.zoom < 4.0) {
+            let percent = toPercent(myState.zoom);
+            console.log("after perc" + percent);
+
+            percent += 20;
+            if (percent >= 400) {
+                return;
+            }
+            document.getElementById('zoom_factor').value = percent + "%";
+            myState.zoom = toFactor(percent);
+        }
+
+        render();
     });
-}
 
-
-function doRender(renderContext, page) {
-    renderInProgress = true;
-    let renderOp = page.render(renderContext);
-    renderOp.promise.then(function() {
-        document.getElementById("canvas_container").style.visibility = "visible";
-    }).finally(function() {
-        renderInProgress = false;
-    })
-}
-
-
-function goPrevPage() {
-    if (myState.pdf == null || myState.currentPage == 1) {
-        return;
-    }
-    myState.currentPage -= 1;
-    document.getElementById("current_page").value = myState.currentPage;
-    render();
-}
-
-function goNextPage() {
-    if (myState.pdf == null || myState.currentPage > myState.pdf._pdfInfo.numPages) {
-        return;
-    }
-    myState.currentPage += 1;
-    document.getElementById("current_page").value = myState.currentPage;
-    render();
-}
-
-function scrollPage(event) {
-    event.preventDefault;
-
-    //scrolling down
-    if (event.deltaY > 0) {
-        goNextPage();
-
-        //scrolling up
-    } else if (event.deltaY < 0) {
-        goPrevPage();
-    }
-}
-
-function enterPageNum(event) {
-    event.preventDefault;
-
-    if (myState.pdf == null) {
-        return;
-    }
-
-    if (event.key == 'Enter') {
-        const desiredPage = document.getElementById('current_page').valueAsNumber;
-
-        if (desiredPage >= 1 && desiredPage <= myState.pdf._pdfInfo.numPages) {
-            myState.currentPage = desiredPage;
-            document.getElementById("current_page").value = desiredPage;
-            render();
-        }
-    }
-}
-
-function enterZoomFactor(event) {
-    event.preventDefault;
-
-    if (myState.pdf == null) {
-        return;
-    }
-
-    if (event.key == 'Enter') {
-        const desiredZoom = document.getElementById('zoom_factor').value;
-
-        let zoomVal = 0;
-        if (desiredZoom.charAt(desiredZoom.length - 1) == '%') {
-            zoomVal = parseInt(desiredZoom.substring(0, desiredZoom.length - 1));
-        } else {
-            zoomVal = parseInt(desiredZoom);
+    document.getElementById('zoom_out').addEventListener('click', (e) => {
+        if (myState.pdf == null || myState.zoom <= 0.4) {
+            return;
         }
 
-        if (zoomVal >= 40 && zoomVal <= 400) {
+        if (myState.zoom > 0.4) {
+            let percent = toPercent(myState.zoom);
 
-            myState.zoom = toFactor(zoomVal);
-            document.getElementById("zoom_factor").value = zoomVal + "%";
+            percent -= 20;
+            if (percent <= 40) {
+                return;
+            }
+            document.getElementById('zoom_factor').value = percent + "%";
+            myState.zoom = toFactor(percent);
 
-            render();
         }
-    }
-}
 
+        render();
+    });
 
-function toPercent(factor) {
-    let strFloat = factor.toString();
-    let strFTimes100 = "";
-    let digits = strFloat.split('.');
-    if (strFloat.length == 3) {
-        if (digits[0] == '0') {
-            strFTimes100 = digits[1] + '0';
-        } else {
-            strFTimes100 = digits[0] + digits[1] + '0';
-        }
-    } else if (strFloat.length == 4) {
-        if (digits[0] == '0') {
-            strFTimes100 = digits[1] + digits[2];
-        } else {
-            strFTimes100 = digits[0] + digits[1] + digits[2];
-        }
-    } else {
-        strFTimes100 = strFloat + "00";
-    }
-    let intTimes100 = parseInt(strFTimes100);
-    return intTimes100;
-}
-
-function toFactor(percentage) {
-    let times10 = percentage * 100;
-    let strDiv100 = (times10 / 100).toString();
-    let strToDecimal = '';
-    if (strDiv100.length == 2) {
-        strToDecimal = "0." + strDiv100.substring(0, 1) + strDiv100.substring(1, 2);
-    } else {
-        strToDecimal = strDiv100.substring(0, 1) + '.' + strDiv100.substring(1, 2) + strDiv100.substring(2, 3);
-    }
-
-    let div100Float = parseFloat(strToDecimal);
-    return div100Float;
-}
-
-//$(document).ready(function() {
-document.getElementById('inputfile').onchange = function(event) {
-    const file = event.target.files[0];
-    const fileReader = new FileReader();
-    fileReader.onload = function() {
-        const typedarray = new Uint8Array(this.result);
-        //pdfjsLib.GlobalWorkerOptions.workerSrc = '//mozilla.github.io/pdf.js/build/pdf.worker.js';
-        const loadingTask = pdfjsLib.getDocument(typedarray);
-        loadingTask.promise.then(pdf => {
-            myState.pdf = pdf;
-            myState.currentPage = 1;
-            document.getElementById("current_page").value = 1;
-            render();
+    function openEmptyPageDialog() {
+        $("#empty_page_dialog").dialog({
+            autoOpen: false,
+            dialogClass: "no-close"
         });
-
-    }
-    fileReader.readAsArrayBuffer(file);
-}
-
-document.getElementById('zoom_in').addEventListener('click', (e) => {
-    if (myState.pdf == null || myState.zoom >= 4.0) {
-        return;
+        $("#create_pdf").click(function() {
+            $("#empty_page_dialog").dialog("open");
+        });
     }
 
-    if (myState.zoom < 4.0) {
-        let percent = toPercent(myState.zoom);
-        console.log("after perc" + percent);
+    async function createPdf() {
+        const pdfDoc = await PDFDocument.create()
 
-        percent += 20;
-        if (percent >= 400) {
-            return;
-        }
-        document.getElementById('zoom_factor').value = percent + "%";
-        myState.zoom = toFactor(percent);
+        const page = pdfDoc.addPage()
+
+        const pdfBytes = await pdfDoc.save()
     }
 
-    render();
+    openEmptyPageDialog();
+
+    document.getElementById('go_previous').addEventListener('click', goPrevPage);
+    document.getElementById('go_next').addEventListener('click', goNextPage);
+
+    document.getElementById('pdf_renderer').addEventListener('wheel', scrollPage);
+
+    document.getElementById('current_page').addEventListener('keyup', enterPageNum);
+    document.getElementById('zoom_factor').addEventListener('keyup', enterZoomFactor);
 });
-
-document.getElementById('zoom_out').addEventListener('click', (e) => {
-    if (myState.pdf == null || myState.zoom <= 0.4) {
-        return;
-    }
-
-    if (myState.zoom > 0.4) {
-        let percent = toPercent(myState.zoom);
-
-        percent -= 20;
-        if (percent <= 40) {
-            return;
-        }
-        document.getElementById('zoom_factor').value = percent + "%";
-        myState.zoom = toFactor(percent);
-
-    }
-
-    render();
-});
-
-document.getElementById('go_previous').addEventListener('click', goPrevPage);
-document.getElementById('go_next').addEventListener('click', goNextPage);
-
-document.getElementById('pdf_renderer').addEventListener('wheel', scrollPage);
-
-document.getElementById('current_page').addEventListener('keyup', enterPageNum);
-document.getElementById('zoom_factor').addEventListener('keyup', enterZoomFactor);
-//});
