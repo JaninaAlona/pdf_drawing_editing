702ac6e0f8eb885adb1bd804fff7077c5d6f84ff
JaninaAlona <jelona@posteo.net>
2021-09-30 00:22:35 +0200

    render all pages at once with multiple canvas

--- js/scripts.js
+++ js/scripts.js
@@ -6,100 +6,77 @@ $(document).ready(function() {
     }
 
     let renderInProgress = false;
+    let numPages = 0;
+    let currentPage = 1;
 
     //reset input fields
     myState.currentPage = 1;
     myState.zoom = 1.0;
     document.getElementById("current_page").value = myState.currentPage;
     document.getElementById('zoom_factor').value = toPercent(myState.zoom);
+    const fileManipButtons = document.querySelector('.file_manip');
+    fileManipButtons.disabled = true;
 
 
-    function render() {
 
-        // The document is loaded here...
-        myState.pdf.getPage(myState.currentPage).then(function(page) {
-
-            let scale = myState.zoom;
-            let viewport = page.getViewport({
-                scale: scale
-            });
-
-            const canvas = document.getElementById('pdf_renderer');
-            const context = canvas.getContext('2d');
-            canvas.height = viewport.height;
-            canvas.width = viewport.width;
-
-            // Render PDF page into canvas context
-            let renderContext = {
-                canvasContext: context,
-                viewport: viewport
-            };
-
-            if (!renderInProgress) {
-                doRender(renderContext, page);
-            }
+    function renderAllPages(page) {
+        let scale = myState.zoom;
+        let viewport = page.getViewport({
+            scale: scale
         });
-    }
 
+        let canvas = document.createElement("canvas");
+        canvas.style.display = "block";
+        canvas.style.marginBottom = "20px";
 
-    function doRender(renderContext, page) {
-        renderInProgress = true;
-        let renderOp = page.render(renderContext);
-        renderOp.promise.then(function() {
-            document.getElementById("canvas_container").style.visibility = "visible";
-        }).finally(function() {
-            renderInProgress = false;
-        })
+        const context = canvas.getContext('2d');
+        canvas.height = viewport.height;
+        canvas.width = viewport.width;
+
+        // Render PDF page into canvas context
+        page.render({
+            canvasContext: context,
+            viewport: viewport
+        });
+
+        document.getElementById('pdf_viewer').appendChild(canvas);
+
+        currentPage++;
+        if (myState.pdf != null && currentPage <= numPages) {
+            myState.pdf.getPage(currentPage).then(renderAllPages);
+        }
     }
 
 
     function goPrevPage() {
-        if (myState.pdf == null || myState.currentPage == 1) {
+        if (myState.currentPage == 1) {
             myState.currentPage = 1;
             document.getElementById('current_page').value = 1;
         } else {
             myState.currentPage -= 1;
             document.getElementById("current_page").value = myState.currentPage;
-            render();
         }
     }
 
     function goNextPage() {
-        if (myState.pdf == null || myState.currentPage >= myState.pdf._pdfInfo.numPages) {
+        if (myState.currentPage >= myState.pdf._pdfInfo.numPages) {
             myState.currentPage = myState.pdf._pdfInfo.numPages;
             document.getElementById('current_page').value = myState.pdf._pdfInfo.numPages;
         } else {
             myState.currentPage += 1;
             document.getElementById("current_page").value = myState.currentPage;
-            render();
-        }
-    }
-
-    function scrollPage(e) {
-        e.preventDefault;
-
-        //scrolling down
-        if (e.deltaY > 0) {
-            goNextPage();
-
-            //scrolling up
-        } else if (e.deltaY < 0) {
-            goPrevPage();
         }
     }
 
     function enterPageNum(e) {
         e.preventDefault;
 
-        if (myState.pdf != null) {
-            if (e.key == 'Enter') {
-                const desiredPage = document.getElementById('current_page').valueAsNumber;
+        if (e.key == 'Enter') {
+            const desiredPage = document.getElementById('current_page').valueAsNumber;
 
-                if (desiredPage >= 1 && desiredPage <= myState.pdf._pdfInfo.numPages) {
-                    myState.currentPage = desiredPage;
-                    document.getElementById("current_page").value = desiredPage;
-                    render();
-                }
+            if (desiredPage >= 1 && desiredPage <= myState.pdf._pdfInfo.numPages) {
+                myState.currentPage = desiredPage;
+                document.getElementById("current_page").value = desiredPage;
             }
         }
     }
@@ -107,26 +84,22 @@ $(document).ready(function() {
     function enterZoomFactor(e) {
         e.preventDefault;
 
-        if (myState.pdf == null) {
-            return;
-        } else {
-            if (e.key == 'Enter') {
-                const desiredZoom = document.getElementById('zoom_factor').value;
+        if (e.key == 'Enter') {
+            const desiredZoom = document.getElementById('zoom_factor').value;
 
-                let zoomVal = 0;
-                if (desiredZoom.charAt(desiredZoom.length - 1) == '%') {
-                    zoomVal = parseInt(desiredZoom.substring(0, desiredZoom.length - 1));
-                } else {
-                    zoomVal = parseInt(desiredZoom);
-                }
+            let zoomVal = 0;
+            if (desiredZoom.charAt(desiredZoom.length - 1) == '%') {
+                zoomVal = parseInt(desiredZoom.substring(0, desiredZoom.length - 1));
+            } else {
+                zoomVal = parseInt(desiredZoom);
+            }
 
-                if (zoomVal >= 40 && zoomVal <= 400) {
+            if (zoomVal >= 40 && zoomVal <= 400) {
 
-                    myState.zoom = toFactor(zoomVal);
-                    document.getElementById("zoom_factor").value = zoomVal + "%";
+                myState.zoom = toFactor(zoomVal);
+                document.getElementById("zoom_factor").value = zoomVal + "%";
 
-                    render();
-                }
+                renderAllPages();
             }
         }
     }
@@ -179,8 +152,13 @@ $(document).ready(function() {
             loadingTask.promise.then(pdf => {
                 myState.pdf = pdf;
                 myState.currentPage = 1;
+                currentPage = 1;
+                myState.zoom = 1;
+                numPages = myState.pdf._pdfInfo.numPages;
                 document.getElementById("current_page").value = 1;
-                render();
+                fileManipButtons.disabled = false;
+                document.getElementById("pdf_viewer").style.visibility = "visible";
+                myState.pdf.getPage(1).then(renderAllPages);
             });
 
         }
@@ -188,10 +166,6 @@ $(document).ready(function() {
     }
 
     document.getElementById('zoom_in').addEventListener('click', (e) => {
-        if (myState.pdf == null || myState.zoom >= 4.0) {
-            return;
-        }
-
         if (myState.zoom < 4.0) {
             let percent = toPercent(myState.zoom);
 
@@ -203,14 +177,10 @@ $(document).ready(function() {
             myState.zoom = toFactor(percent);
         }
 
-        render();
+        renderAllPages();
     });
 
     document.getElementById('zoom_out').addEventListener('click', (e) => {
-        if (myState.pdf == null || myState.zoom <= 0.4) {
-            return;
-        }
-
         if (myState.zoom > 0.4) {
             let percent = toPercent(myState.zoom);
 
@@ -222,16 +192,11 @@ $(document).ready(function() {
             myState.zoom = toFactor(percent);
 
         }
-
-        render();
+        renderAllPages();
     });
-
 
     document.getElementById('go_previous').addEventListener('click', goPrevPage);
     document.getElementById('go_next').addEventListener('click', goNextPage);
-
-    document.getElementById('pdf_renderer').addEventListener('wheel', scrollPage);
-
     document.getElementById('current_page').addEventListener('keyup', enterPageNum);
     document.getElementById('zoom_factor').addEventListener('keyup', enterZoomFactor);
 });
